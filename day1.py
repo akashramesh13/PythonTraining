1cbe75d271334e7d8f844d9a2f89344c85d53d08
akashramesh13 <akashramesh13@gmail.com>
2020-10-06 10:28:39 +0530

    Day one tasks completed

---
+++ day1.py
@@ -0,0 +1,12 @@
+#Greeting program
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+
+#Voting progrma
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote!")
+else:
+    print("Sorry! You are ineligible!")
+
+
