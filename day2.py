79641c0e67968e3570d30e194121e8a1df2ea729
akashramesh13 <akashramesh13@gmail.com>
2020-10-06 15:03:48 +0530

    Completed day 2 tasks

--- day2.py
+++ day2.py
@@ -10,3 +10,29 @@ for i in range(1,N+1):
         print("Buzz")
     else:
         print(i)
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Guessing game
+
+#Max number of attempts is 10
+# The else block will execute if the user doesn't guess in 10 tries since it would've "break"-ed out otherwise
+actual_number = 76
+tries = 1
+while(tries <= 10):
+    guessed_number = int(input("Guess the number:\n"))
+    if(actual_number == guessed_number):
+        print(f"Hooray!!! You've guessed the number in {tries} tries")
+        break
+    elif(actual_number > guessed_number):
+        print("Try a larger value")
+    else:
+        print("Try a smaller value")
+    tries += 1
+#Goes here if the program does not break out therefore meaning that the user has failed to guess the number in 10 tries
+else:
+    print("You have used all 10 tries! Sorry!")
+
+    
