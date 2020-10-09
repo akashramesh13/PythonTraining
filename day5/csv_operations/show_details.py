a8e44c290bc8b19fa74d6c79b0e70b4bdac11c59
akashramesh13 <akashramesh13@gmail.com>
2020-10-09 13:48:40 +0530

    fixed a small bug

--- day5/csv_operations/show_details.py
+++ day5/csv_operations/show_details.py
@@ -1,10 +1,7 @@
 import csv
-# vscode is considering my python path as default, works fine in IDE
-#  so had to change path here.
-path = "Documents/python_git/day5/csv_operations/participants.csv"
-with open(path) as csv_file:  # read mode by default
+with open("day5/csv_operations/participants.csv") as csv_file:  # read mode by default
     csv_reader = csv.DictReader(csv_file)
     for participant in csv_reader:
         print(
-            f"{participant['name']} has {participant['experience(in_years)']} experience in "
+            f"{participant['name']} has {participant['experience(in_years)']} years of experience in "
             f"{participant['mastered_language']}")
