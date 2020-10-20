3b806e675ddf4bb7eb0bcf73a9b6cbeb2ae0029c
akashramesh13 <akashramesh13@gmail.com>
2020-10-20 08:01:30 +0530

    created django app - vehicles

---
+++ day11/sampleapp/mysite/vehicles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Vehicle(models.Model):
+
+    def __str__(self):
+        return f"{self.vehicle_type} released on: {self.release_date}"
+
+    vehicle_type = models.CharField(max_length=200)
+    release_date = models.DateTimeField('date of release')
+
+
+class Brand(models.Model):
+
+    def __str__(self):
+        return f"{self.vehicle} -> {self.brand_name} costs {self.price}"
+
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    brand_name = models.CharField(max_length=200)
+    price = models.FloatField(default=0)
